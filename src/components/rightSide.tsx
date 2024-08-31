66bb758d9ded9f96b070bc0b3f717b73b900570f
dev-YashJain <eng.yashjain@gmail.com>
2024-08-31 18:18:45 +0530

    rightside

--- src/components/rightSide.tsx
+++ src/components/rightSide.tsx
@@ -2,118 +2,99 @@ import '../components/rightSide.css';
 
 const RightSide = () => {
     return (
-        <div>
-            right side
+        <div className="parentContainer">
+            <div className="rightSideHome">
+                <div className="profileImage">
+                    <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
+                </div>
+                <div className="texts">
+                    <div className="text1">
+                        Jacqueline Fernandez
+                    </div>
+                    <div className="text2">
+                        Jacqueline
+                    </div>
+                </div>
+                <div className="buttons">
+                    Switch
+                </div>
+
+            </div>
+            <div className="separatorText">
+                <div className="text1">Suggested for you</div>
+                <div className="text2">See all</div>
+            </div>
+            <div className="rightSideHome">
+                <div className="profileImage">
+                    <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
+                </div>
+                <div className="texts">
+                    <div className="text1">
+                        Jacqueline Fernandez
+                    </div>
+                    <div className="text2">
+                        Jacqueline
+                    </div>
+                </div>
+                <div className="buttons">
+                    Switch
+                </div>
+
+            </div>
+
+            <div className="rightSideHome">
+                <div className="profileImage">
+                    <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
+                </div>
+                <div className="texts">
+                    <div className="text1">
+                        Jacqueline Fernandez
+                    </div>
+                    <div className="text2">
+                        Jacqueline
+                    </div>
+                </div>
+                <div className="buttons">
+                    Switch
+                </div>
+
+            </div>
+            <div className="rightSideHome">
+                <div className="profileImage">
+                    <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
+                </div>
+                <div className="texts">
+                    <div className="text1">
+                        Jacqueline Fernandez
+                    </div>
+                    <div className="text2">
+                        Jacqueline
+                    </div>
+                </div>
+                <div className="buttons">
+                    Switch
+                </div>
+
+            </div>
+            <div className="rightSideHome">
+                <div className="profileImage">
+                    <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
+                </div>
+                <div className="texts">
+                    <div className="text1">
+                        Jacqueline Fernandez
+                    </div>
+                    <div className="text2">
+                        Jacqueline
+                    </div>
+                </div>
+                <div className="buttons">
+                    Switch
+                </div>
+
+            </div>
         </div>
-    //     <div className="parentContainer">
-    //         <div className="rightSideHome">
-    //             <div className="profileImage">
-    //                 <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
-    //             </div>
-    //             <div className="texts">
-    //                 <div className="text1">
-    //                     Jacqueline Fernandez
-    //                 </div>
-    //                 <div className="text2">
-    //                     Jacqueline
-    //                 </div>
-    //             </div>
-    //             <div className="buttons">
-    //                 Switch
-    //             </div>
 
-    //         </div>
-    //         <div className="separatorText">
-    //             <div className="text1">Suggested for you</div>
-    //             <div className="text2">See all</div>
-    //         </div>
-    //         <div className="rightSideHome">
-    //             <div className="profileImage">
-    //                 <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
-    //             </div>
-    //             <div className="texts">
-    //                 <div className="text1">
-    //                     Jacqueline Fernandez
-    //                 </div>
-    //                 <div className="text2">
-    //                     Jacqueline
-    //                 </div>
-    //             </div>
-    //             <div className="buttons">
-    //                 Switch
-    //             </div>
-
-    //         </div>
-    //         <div className="rightSideHome">
-    //             <div className="profileImage">
-    //                 <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
-    //             </div>
-    //             <div className="texts">
-    //                 <div className="text1">
-    //                     Jacqueline Fernandez
-    //                 </div>
-    //                 <div className="text2">
-    //                     Jacqueline
-    //                 </div>
-    //             </div>
-    //             <div className="buttons">
-    //                 Switch
-    //             </div>
-
-    //         </div>
-    //         <div className="rightSideHome">
-    //             <div className="profileImage">
-    //                 <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
-    //             </div>
-    //             <div className="texts">
-    //                 <div className="text1">
-    //                     Jacqueline Fernandez
-    //                 </div>
-    //                 <div className="text2">
-    //                     Jacqueline
-    //                 </div>
-    //             </div>
-    //             <div className="buttons">
-    //                 Switch
-    //             </div>
-
-    //         </div>
-    //         <div className="rightSideHome">
-    //             <div className="profileImage">
-    //                 <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
-    //             </div>
-    //             <div className="texts">
-    //                 <div className="text1">
-    //                     Jacqueline Fernandez
-    //                 </div>
-    //                 <div className="text2">
-    //                     Jacqueline
-    //                 </div>
-    //             </div>
-    //             <div className="buttons">
-    //                 Switch
-    //             </div>
-
-    //         </div>
-    //         <div className="rightSideHome">
-    //             <div className="profileImage">
-    //                 <img src="https://assets.vogue.in/photos/601bfddd3514c40d2b37e596/2:3/w_2560%2Cc_limit/jacqueline%2520fernandez%2520makeup%2520skincare.jpg" alt="" />
-    //             </div>
-    //             <div className="texts">
-    //                 <div className="text1">
-    //                     Jacqueline Fernandez
-    //                 </div>
-    //                 <div className="text2">
-    //                     Jacqueline
-    //                 </div>
-    //             </div>
-    //             <div className="buttons">
-    //                 Switch
-    //             </div>
-
-    //         </div>
-    //     </div>
-    // 
     )
 }
 

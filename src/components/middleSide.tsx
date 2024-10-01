be3ac4d92e33244539fbe2019f6a267f03da9581
dev-YashJain <eng.yashjain@gmail.com>
2024-10-01 16:00:58 +0530

    CardChanges

--- src/components/middleSide.tsx
+++ src/components/middleSide.tsx
@@ -1,28 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import storyDetails from '../storyDetails.json'; // Adjust the path if necessary
 import '../components/middleSide.css';
-import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
-import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
-import LikeIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import CommentIcon from '@mui/icons-material/ModeCommentOutlined';
-import ShareIcon from '@mui/icons-material/IosShareRounded';
-import BookmarkIcon from '@mui/icons-material/BookmarkBorderRounded';
 import whiteLogo from '../assets/whiteLogo.png';
 import instagramLogo from '../assets/Instagram_logo.svg.png';
 import NotificationIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import StoryViewer from './story'; // Import StoryViewer component
+import Card from './postCard'; // Import the Card component
 
 const MiddleSide: React.FC = () => {
     const [darkMode, setDarkMode] = useState<boolean>(() => {
         const savedMode = localStorage.getItem('darkMode');
         return savedMode === 'true';
     });
-
-    const [currentImageIndexes, setCurrentImageIndexes] = useState<number[]>(() =>
-        storyDetails.storyDetails.map(() => 0)
-    );
 
     const [isStoryOpen, setIsStoryOpen] = useState(false);
     const [selectedStoryIndex, setSelectedStoryIndex] = useState<number>(0);
@@ -61,30 +50,6 @@ const MiddleSide: React.FC = () => {
         setIsStoryOpen(false);
     };
 
-    const handlePrevImage = (postIndex: number) => {
-        setCurrentImageIndexes((prevIndexes) =>
-            prevIndexes.map((index, i) =>
-                i === postIndex
-                    ? index === 0
-                        ? storyDetails.storyDetails[postIndex].profile_url.length - 1
-                        : index - 1
-                    : index
-            )
-        );
-    };
-
-    const handleNextImage = (postIndex: number) => {
-        setCurrentImageIndexes((prevIndexes) =>
-            prevIndexes.map((index, i) =>
-                i === postIndex
-                    ? index === storyDetails.storyDetails[postIndex].profile_url.length - 1
-                        ? 0
-                        : index + 1
-                    : index
-            )
-        );
-    };
-
     return (
         <div className='middleSidePart'>
             {/* Top logo bar */}
@@ -117,93 +82,13 @@ const MiddleSide: React.FC = () => {
             )}
 
             {/* Post Section */}
-            {storyDetails.storyDetails.map((story, postIndex) => {
-                const currentImageIndex = currentImageIndexes[postIndex];
-
-                return (
-                    <div className="postSection" key={postIndex}>
-                        <div className="post">
-                            {/* Post Header */}
-                            <div className="postHeader">
-                                <div className="profilePhoto">
-                                    <img src={story.profile_url[0]} alt={story.name} />
-                                </div>
-                                <div className="profileName">{story.name}</div>
-                                <div className="blueTick">
-                                    <CheckCircleOutlineOutlinedIcon style={{ color: 'blue', fontSize: '16px' }} />
-                                </div>
-                                <div className="time">. 10w .</div>
-                                <div className="info">
-                                    <MoreHorizOutlinedIcon style={{ fontSize: '20px' }} />
-                                </div>
-                            </div>
-
-                            {/* Post Images and Dots */}
-                            <div className="postImages">
-                                {/* Only show left arrow if it's not the first image */}
-                                {story.profile_url.length > 1 && currentImageIndex > 0 && (
-                                    <ArrowBackIosNewIcon
-                                        className="prevArrow"
-                                        onClick={() => handlePrevImage(postIndex)}
-                                    />
-                                )}
-
-                                {/* Current Image */}
-                                <img
-                                    src={story.profile_url[currentImageIndex]}
-                                    alt={`Post image ${currentImageIndex + 1}`}
-                                    className="postImage active"
-                                />
-
-                                {/* Only show right arrow if it's not the last image */}
-                                {story.profile_url.length > 1 && currentImageIndex < story.profile_url.length - 1 && (
-                                    <ArrowForwardIosIcon
-                                        className="nextArrow"
-                                        onClick={() => handleNextImage(postIndex)}
-                                    />
-                                )}
-
-                                {/* Dots Indicator */}
-                                {story.profile_url.length > 1 && (
-                                    <div className="dotsContainer">
-                                        {story.profile_url.map((_, dotIndex) => (
-                                            <span
-                                                key={dotIndex}
-                                                className={`dot ${dotIndex === currentImageIndex ? 'activeDot' : ''}`}
-                                            ></span>
-                                        ))}
-                                    </div>
-                                )}
-                            </div>
-
-                            {/* Post Footer */}
-                            <div className="postFooter">
-                                <div className="footerIcons">
-                                    <div className="leftIcons">
-                                        <LikeIcon className='icon' />
-                                        <CommentIcon className='icon' />
-                                        <ShareIcon className='icon' />
-                                    </div>
-                                    <BookmarkIcon className='bookmark' />
-                                </div>
-                                <br />
-                                <div className="footerLikes">68,321 likes</div>
-                                <div className="footerCaption">
-                                    <div className="footerProfileName">Tom Hanks</div>
-                                    <div className="blueTick">
-                                        <CheckCircleOutlineOutlinedIcon style={{ color: 'blue', fontSize: '16px' }} />
-                                    </div>
-                                    <div className="captionText">Strings is all you need.</div>
-                                </div>
-                                <br />
-                                <div className="viewComment">View all 681 comments</div>
-                                <div className="addComment">Add a comment...</div>
-                            </div>
-                            <hr className="postSeparator" />
-                        </div>
-                    </div>
-                );
-            })}
+            {storyDetails.storyDetails.map((story, postIndex) => (
+                <Card
+                    key={postIndex}
+                    profile_url={story.profile_url}
+                    name={story.name}
+                />
+            ))}
         </div>
     );
 };
---
+++ src/components/postCard.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import '../components/middleSide.css'; // Ensure the path is correct based on your project structure
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import LikeIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import CommentIcon from '@mui/icons-material/ModeCommentOutlined';
+import ShareIcon from '@mui/icons-material/IosShareRounded';
+import BookmarkIcon from '@mui/icons-material/BookmarkBorderRounded';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import '../components/postCard.css'
+
+interface CardProps {
+    profile_url: string[];
+    name: string;
+}
+
+const Card: React.FC<CardProps> = ({ profile_url, name }) => {
+    const [currentImageIndex, setCurrentImageIndex] = React.useState<number>(0);
+
+    const handlePrevImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? profile_url.length - 1 : prevIndex - 1
+        );
+    };
+
+    const handleNextImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === profile_url.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    return (
+        <div className="postSection">
+            <div className="post">
+                {/* Post Header */}
+                <div className="postHeader">
+                    <div className="profilePhoto">
+                        <img src={profile_url[0]} alt={name} />
+                    </div>
+                    <div className="profileName">{name}</div>
+                    <div className="blueTick">
+                        <CheckCircleOutlineOutlinedIcon style={{ color: 'blue', fontSize: '16px' }} />
+                    </div>
+                    <div className="time">. 10w .</div>
+                    <div className="info">
+                        <MoreHorizOutlinedIcon style={{ fontSize: '20px' }} />
+                    </div>
+                </div>
+
+                {/* Post Images and Dots */}
+                <div className="postImages">
+                    {/* Left Arrow */}
+                    {profile_url.length > 1 && currentImageIndex > 0 && (
+                        <ArrowBackIosNewIcon
+                            className="prevArrow"
+                            onClick={handlePrevImage}
+                        />
+                    )}
+
+                    {/* Current Image */}
+                    <img
+                        src={profile_url[currentImageIndex]}
+                        alt={`Post image ${currentImageIndex + 1}`}
+                        className="postImage active"
+                    />
+
+                    {/* Right Arrow */}
+                    {profile_url.length > 1 && currentImageIndex < profile_url.length - 1 && (
+                        <ArrowForwardIosIcon
+                            className="nextArrow"
+                            onClick={handleNextImage}
+                        />
+                    )}
+
+                    {/* Dots Indicator */}
+                    {profile_url.length > 1 && (
+                        <div className="dotsContainer">
+                            {profile_url.map((_, dotIndex) => (
+                                <span
+                                    key={dotIndex}
+                                    className={`dot ${dotIndex === currentImageIndex ? 'activeDot' : ''}`}
+                                ></span>
+                            ))}
+                        </div>
+                    )}
+                </div>
+
+                {/* Post Footer */}
+                <div className="postFooter">
+                    <div className="footerIcons">
+                        <div className="leftIcons">
+                            <LikeIcon className='icon' />
+                            <CommentIcon className='icon' />
+                            <ShareIcon className='icon' />
+                        </div>
+                        <BookmarkIcon className='bookmark' />
+                    </div>
+                    <br />
+                    <div className="footerLikes">68,321 likes</div>
+                    <div className="footerCaption">
+                        <div className="footerProfileName">Tom Hanks</div>
+                        <div className="blueTick">
+                            <CheckCircleOutlineOutlinedIcon style={{ color: 'blue', fontSize: '16px' }} />
+                        </div>
+                        <div className="captionText">Strings is all you need.</div>
+                    </div>
+                    <br />
+                    <div className="viewComment">View all 681 comments</div>
+                    <div className="addComment">Add a comment...</div>
+                </div>
+                <hr className="postSeparator" />
+            </div>
+        </div>
+    );
+};
+
+export default Card;
